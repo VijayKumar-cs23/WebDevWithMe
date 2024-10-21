45300d6965dabb5818d0856980146ff4dc94c074
Vijay Kumar <vijay.kumar_cs23@gla.ac.in>
2024-10-21 19:23:40 +0530

    initial commit

---
+++ Basics/basics.js
@@ -0,0 +1,7 @@
+.js is used.
+console --> bassed on REPL - Read,Evaluate,Print,Loop.
+            consol window basically used for practice purpose 
+            To clear the window = ctrl+L 
+            upword direction key is used to review previous elements 
+            same as lower direction is used to review current elements.
+          
