e7798b501346d5eac0e7ebcfc73a985026c01841
Vijay Kumar <vijay.kumar_cs23@gla.ac.in>
2024-10-22 19:29:26 +0530

    current

--- Basics/basics.js
+++ Basics/basics.js
@@ -5,3 +5,65 @@ console --> bassed on REPL - Read,Evaluate,Print,Loop.
             upword direction key is used to review previous elements 
             same as lower direction is used to review current elements.
           
+
+variable --> variable is container use as a storage container.
+             some values it stores and it's values are as upcomming operations.   
+             
+Data types --> 
+
+1.Premitive data types-> Number
+                         Boolean
+                         String
+                         undefined
+                         null
+                         BigInt
+                         Symbol
+
+typeof -> is used to show the type of the variable.
+ex- a=25
+    typeof a = 'number'
+
+    name = "TONY STARK"
+    typeof name = 'string' 
+
+NUMBER -> {2,-34,3.5,-54.7,0} are Validi.
+
+Operations-> sum -> a+b
+             sub -> a-b
+             mul -> a*b
+             div -> a/b
+             modulo -> a%b = remainder
+             exponentiation(power) a**B = means A of power b .
+             
+
+NaN -> NOT A NUMBER  -> it represents a number but invalid .
+       ex- 0/0
+           NaN+1
+           NaN-5
+
+Operator Precedence -> ()
+                       **
+                       *, /, %
+                       +, -
+
+let Keyword -> Syntax of declaring variables.
+               let a = 5;
+               let b = 1;
+               let sum = a + b;
+
+               let side = 4
+               let areaOfSquar = 4**2;
+
+
+const Keyword -> values of constants can not be changed with reassignment & they can not be re-declared.
+                 ex- const year = 2024;
+                     const pi  = 3.14;
+                     xonst g = 9.8;
+
+var Keyword -> to define variables.
+              ex = var = 9;
+              
+
+
+
+             
